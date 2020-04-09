01ac64eb6f21d1b2da913b8720813b47fe29eb2c
bobliugq <liuguangqun.bob@gmail.com>
2020-04-09 16:42:26 +0800

    update the problem 674

--- Array/674.Longest_Continuous_Increasing_Subsequence.cpp
+++ Array/674.Longest_Continuous_Increasing_Subsequence.cpp
@@ -1,22 +1,18 @@
 // Data:        2020.4.8
 // Topic:       Array + GA
 // Todo:        Time and space complexity
+
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
-        int maxArr = 1;
+        if (nums.empty()) return 0;
         int n = 1;
+        int maxLen = 1;
         for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i - 1] < nums[i]) {
-                ++n;
-            } else {
-                n = 1;
-            }
-            maxArr = max(maxArr, n);
+            if (nums[i] > nums[i - 1]) ++n;
+            else n = 1;
+            maxLen = max(maxLen, n);
         }
-        return maxArr;
+        return maxLen;
     }
 };
