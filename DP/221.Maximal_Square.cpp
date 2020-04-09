ed038c24672ee5664cd851648cc4e7eb367e812f
bobliugq <liuguangqun.bob@gmail.com>
2020-04-09 16:10:56 +0800

    DP problem

---
+++ DP/221.Maximal_Square.cpp
@@ -0,0 +1,25 @@
+// Date:        2020.4.8
+// Topic:       DP + Array
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
+        int n = matrix.size(), m = matrix[0].size();
+        int maxLen = 0;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (!i || !j || matrix[i][j] == '0') {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                maxLen = max(maxLen, dp[i][j]);
+            }
+        }
+        return maxLen * maxLen;
+    }
+};
